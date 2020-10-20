0f1a2b67652c756bb5a498034f47499b37aea260
Technicallyaustin <Technicallyaustin@gmail.com>
2020-10-20 14:45:31 -0700

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,27 @@
+class StudentsController < ApplicationController
+
+def index
+end
+
+def new
+end
+
+def show
+end
+
+
+def create 
+    @student = Student.new(parmas)
+end
+
+def edit
+end
+
+def update
+end
+
+
+
+
+
+end
---
+++ db/migrate/002_create_school_classes.rb
@@ -0,0 +1,8 @@
+class CreateSchoolClasses < ActiveRecord::Migration
+    def change
+        create_table :schoolclass do |t|
+            t.string :title
+            t.integer :room_number
+        end
+    end
+end
