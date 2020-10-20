ff5a77480d21704b6e0a71b75ab7830a66412cd2
Technicallyaustin <Technicallyaustin@gmail.com>
2020-10-20 15:01:58 -0700

    Done.

---
+++ app/controllers/schoolclasses_controller.rb
@@ -0,0 +1,33 @@
+class SchoolclassesController < ApplicationController
+
+def index
+@schoolclasses = SchoolClass.all 
+end
+
+def new
+    @schoolclass = SchoolClass.new
+end
+
+def show
+    @schoolclass = SchoolClass.find(params[:id])
+end
+
+
+def create 
+   @schoolclass = SchoolClass.new
+   @schoolclass.title = params[:title]
+   @schoolclass.room_number = params[:room_number]
+   @schoolclass.save
+   redirect_to schoolclasses_path(@schoolclass)
+end
+
+def edit
+    @schoolclass = SchoolClass.find(params[:id])
+end
+
+def update
+    @schoolclass = SchoolClass.find(params[:id])
+    @schoolclass.update
+end
+
+end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,27 +1,32 @@
 class StudentsController < ApplicationController
 
 def index
+    @students = Student.all
 end
 
 def new
+    @student = Student.new
 end
 
 def show
+    @student = Student.find(params[:id])
 end
 
-
 def create 
-    @student = Student.new(parmas)
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to students_path(@student)
 end
 
 def edit
+    @student = Student.find(params[:id])
 end
 
 def update
+    @student = Student.find(params[:id])
+    @student.update
 end
-
-
-
-
 
 end
